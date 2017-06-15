82a73d5bb1ce3df272d46b8ce43eb64595e61c90
Edward Zarecor <ed@edx.org>
2017-06-14 21:11:48 -0400

    explict template settings

--- notifier/settings.py
+++ notifier/settings.py
@@ -31,6 +31,26 @@ INSTALLED_APPS = (
 
 SERVICE_NAME = 'notifier'
 
+# Template Settings
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 # Misc. Notifier Formatting
 
 FORUM_DIGEST_EMAIL_SENDER = os.getenv('FORUM_DIGEST_EMAIL_SENDER', 'notifications@example.org')
